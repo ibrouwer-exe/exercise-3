3232ecd6168649946a6f7679e4313903122990e8
ibrouwer-exe <i.brouwer@uleth.ca>
2023-11-26 19:33:26 -0700

    finished basic parts of site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 
-    $("#cozyin").on("click", function() {
+$("#vibin").on("click", function() {
+    $(".bear").html("<img src='imgs/vibin bear.png' alt='vibin bear'>")
+    console.log("clicked");
+});
+$("#cozyin").on("click", function() {
+        $(".bear").html("<img src='imgs/cozyin-bear.png' alt='cozyin bear'>")
         console.log("clicked");
     });
-
+    $("#listenin").on("click", function() {
+        $(".bear").html("<img src='imgs/listenin bear.png' alt='listenin bear'>")
+        console.log("clicked");
+    });
+    $("#singin").on("click", function() {
+        $(".bear").html("<img src='imgs/singin bear.png' alt='singin bear'>")
+        console.log("clicked");
+    });
+    $("#rollin").on("click", function() {
+        $(".bear").html("<img src='imgs/rollin bear.png' alt='they see me rollin, they hatin'>")
+        console.log("clicked");
+    });
